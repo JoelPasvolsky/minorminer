0b089411e051d3a4b99a97373c94674b285ba388
jpasvolsky <jpasvolsky@dwavesys.com>
2020-09-01 10:18:40 -0700

    Set sys path to sdk path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,8 @@
 import subprocess
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-config_directory = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.abspath(
-    os.path.join(config_directory, '..)))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 _PY2 = sys.version_info.major == 2
 
